49bd38059a6e10e3d8ffad1bf2a42c41fc76916d
solo-leveler <shinichiizumi@protonmail.ch>
2026-02-15 15:52:06 +0530

    refactor(home) : add authentication check and display resumes

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,42 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for Resume Writing" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect (() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+  
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume Progress</h1>
+          <h2>Review your resume and get AI-powered feedback.</h2>
+        </div>
+        {resumes.length && (
+          <div className="resumes-section">
+            {resumes.map((resume : Resume) => {
+              return <ResumeCard key={resume.id} resume={resume} />;
+            })}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
